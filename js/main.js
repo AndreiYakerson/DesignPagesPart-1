5e8cb5c60d1e800a8a97993c72ef451d96c37d41
AndreiYakerson <ckem1392@gmail.com.com>
2025-04-13 20:42:30 +0300

    Add side bar navigation

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function onClickMenu() {
+    const menu = document.querySelector('.main-navigation')
+    const screen = document.querySelector('.main-screen')
+    menu.classList.toggle('show')
+    screen.classList.toggle('drop-down')
+}
